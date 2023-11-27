c48db9e7ff35f93019e24d19fa8201c8db84cc8d
Miroslav Krastev <miroslav.krastev@ipsos.com>
2023-11-28 00:46:27 +0800

    error handling added

--- gui.py
+++ gui.py
@@ -28,44 +28,58 @@ while True:
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     event, values = window.read()
     print(1, "event: ", event)
+
+    if event == sg.WIN_CLOSED:
+        break
+
     if event == "ListboxClick":
         print("selected: ", values['ListboxClick'][0])
         window['new_todo'].update(value=values['ListboxClick'][0])
 
     if event.lower() == "add":
-        todos = functions.get_todos()
-        new_todo = values['new_todo'] + "\n"
-        print(2, "adding: ", str(values['new_todo']).replace("\n", ""))
-        todos.append(new_todo)
-        functions.write_todos(todos)
-        window['new_todo'].update(value='')
-        window['ListboxClick'].update(values=todos)
+        new_todo_stripped = values['new_todo'].strip()
+        print(2, "add: ", new_todo_stripped)
+        if not new_todo_stripped:
+            sg.popup("No item entered yet!")
+        else:
+            new_todo = new_todo_stripped + "\n"
+            todos = functions.get_todos()
+            todos.append(new_todo)
+            functions.write_todos(todos)
+            window['new_todo'].update(value='')
+            window['ListboxClick'].update(values=todos)
 
     if event.lower() == "edit":
         print(2, "replace: ", values['ListboxClick'])
-        todo_to_change = values['ListboxClick'][0]
-        print(3, "with: ", str(values['new_todo']).replace("\n", ""))
-        new_todo = values['new_todo'] + "\n"
-        todos = functions.get_todos()
-        todo_to_change_index = todos.index(todo_to_change)
-        todos[todo_to_change_index] = new_todo
-        functions.write_todos(todos)
-        window['ListboxClick'].update(values=todos)
+        try:
+            todo_to_change = values['ListboxClick'][0]
+            print(3, "with: ", str(values['new_todo']).replace("\n", ""))
+            new_todo = values['new_todo'] + "\n"
+            todos = functions.get_todos()
+            todo_to_change_index = todos.index(todo_to_change)
+            todos[todo_to_change_index] = new_todo
+            functions.write_todos(todos)
+            window['new_todo'].update(value='')
+            window['ListboxClick'].update(values=todos)
+        except IndexError:
+            sg.popup("No item selected!")
+
 
     if event.lower() == "complete":
         print(2, "complete: ", values['ListboxClick'])
-        todo_to_complete = values['ListboxClick'][0]
-        todos = functions.get_todos()
-        # todo_to_complete_index = todos.index(todo_to_complete)
-        todos.remove(todo_to_complete)
-        functions.write_todos(todos)
-        window['ListboxClick'].update(values=todos)
-        window['new_todo'].update(value='')
+        try:
+            todo_to_complete = values['ListboxClick'][0]
+            todos = functions.get_todos()
+            # todo_to_complete_index = todos.index(todo_to_complete)
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['ListboxClick'].update(values=todos)
+            window['new_todo'].update(value='')
+        except IndexError:
+            sg.popup("No item selected!")
 
     if event.lower() == "exit":
         break
 
-    if event == sg.WIN_CLOSED:
-        break
 
 window.close()
